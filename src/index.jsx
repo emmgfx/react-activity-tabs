a1594c263c5b589ca3eaa55dd984d247f1defcce
Josep Viciana <emmgfx@gmail.com>
2025-10-06 10:02:17 +0200

    activeClassName and useActiveTab

--- src/index.jsx
+++ src/index.jsx
@@ -19,10 +19,14 @@ export const TabsList = ({ className, children }) => {
   return <div className={className}>{children}</div>;
 };
 
-export const TabsTab = ({ tabId, className, children }) => {
-  const { setActiveTab } = useContext(TabsContext);
+export const TabsTab = ({ tabId, className, activeClassName, children }) => {
+  const { activeTab, setActiveTab } = useContext(TabsContext);
+  const isActive = activeTab === tabId;
   return (
-    <button className={className} onClick={() => setActiveTab(tabId)}>
+    <button
+      className={isActive && activeClassName ? activeClassName : className}
+      onClick={() => setActiveTab(tabId)}
+    >
       {children}
     </button>
   );
@@ -36,4 +40,9 @@ export const TabsPanel = ({ tabId, className, children }) => {
   const { activeTab } = useContext(TabsContext);
   if (activeTab !== tabId) return null;
   return <div className={className}>{children}</div>;
+};
+
+export const useActiveTab = () => {
+  const { activeTab } = useContext(TabsContext);
+  return activeTab;
 };
